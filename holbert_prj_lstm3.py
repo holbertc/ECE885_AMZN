07a951606af6bf2abae75f51ecb327a03e29993f
holbertc <holbert.christopher@gmail.com>
2017-05-01 18:45:10 -0400

    Update holbert_prj_lstm3.py

--- holbert_prj_lstm3.py
+++ holbert_prj_lstm3.py
@@ -37,8 +37,8 @@ tokenizer.fit_on_texts(train['Reviews'])
 sequences_train = tokenizer.texts_to_sequences(train['Reviews'])
 sequences_test = tokenizer.texts_to_sequences(test['Reviews'])
 
-X_train = sequence.pad_sequences(sequences_train, maxlen=40)
-X_test = sequence.pad_sequences(sequences_test, maxlen=40)
+X_train = sequence.pad_sequences(sequences_train, maxlen=1)
+X_test = sequence.pad_sequences(sequences_test, maxlen=1)
 
 Y_train = np_utils.to_categorical(y_train, 5)
 Y_test = np_utils.to_categorical(y_test, 5)
